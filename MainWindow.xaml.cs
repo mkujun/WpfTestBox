0786d03c058a98d0a4f0b0e54e56ea55f74e2408
mkujun <kujundzic.marko@hotmail.com>
2024-01-18 12:56:34 +0100

    MainWindow calls two new windows on a button click

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfTestBox.Views;
 
 namespace WpfTestBox
 {
@@ -23,6 +24,18 @@ namespace WpfTestBox
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenWindow1_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        private void OpenWindow2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
         }
     }
 }
